676611b266d9b0e1887064f3c335a2659cb8e30b
Sudo Space <abolfazleffati1381@gmail.com>
2025-04-25 10:48:46 +0330

    Prevent sending YouTube playlists

--- src/middlewares/checker.ts
+++ src/middlewares/checker.ts
@@ -14,24 +14,39 @@ checker.on("message", (ctx, next) => {
   const user = ctx.session.user;
   const link = ctx.message?.text as string;
 
-  if (!link || !link.match(/^https?:\/\//))
+  if (!link || !link.match(/^https?:\/\//)) {
     return ctx.reply("Need a link to download media.");
+  }
 
   const domain = link.match(/https?:\/\/([^\/]+)/)?.at(1) as string;
 
-  if (domain == "pin.it" || domain == "pinterest.com")
-    user!.media = { type: "pin", url: link };
-  else if (domain == "youtu.be" || domain == "youtube.com")
+  if (domain == "youtu.be" || domain == "youtube.com") {
+    if (link.includes("list=")) {
+      return ctx.reply("Playlist is not support currently.");
+    }
     user!.media = { type: "yt", url: link };
-  else if (domain == "music.youtube.com")
+  }
+  else if (domain == "music.youtube.com") {
+    if (link.includes("list=")) {
+      return ctx.reply("Playlist is not support currently.");
+    }
     user!.media = { type: "ytm", url: link };
-  else if (domain == "vm.tiktok.com" || domain == "tiktok.com")
+  }
+  else if (domain == "pin.it" || domain == "pinterest.com") {
+    user!.media = { type: "pin", url: link };
+  }
+  else if (domain == "vm.tiktok.com" || domain == "tiktok.com") {
     user!.media = { type: "tt", url: link };
-  else if (domain == "www.instagram.com" || domain == "instagram.com")
+  }
+  else if (domain == "www.instagram.com" || domain == "instagram.com") {
     user!.media = { type: "ig", url: link };
-  else if (domain == "soundcloud.com" || domain == "on.soundcloud.com")
+  }
+  else if (domain == "soundcloud.com" || domain == "on.soundcloud.com") {
     user!.media = { type: "sc", url: link };
-  else return ctx.reply("Unsupported platform! ❌");
+  }
+  else {
+    return ctx.reply("Unsupported platform! ❌");
+  }
   next();
 });
 
